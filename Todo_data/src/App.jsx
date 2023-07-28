037034b5e43e1bdece2f36b8afefdedca3122acb
g-dolidze <giorgi.dolidze55@gmail.com>
2023-07-28 23:29:58 +0400

    fixed mistakes

--- Todo_data/src/App.jsx
+++ Todo_data/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import "./app.css";
+import "./app.scss";
 
 const notify = () => toast("New Todo!");
 
